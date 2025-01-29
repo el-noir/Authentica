357ec9664fce2627c1eb4945c28ed8641aefe0ce
mudasirshah434 <mudasirshah9777@gmail.com>
2025-01-30 00:46:36 +0500

    dotenv for loading the environment variables

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import {app} from './app.js';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: './.env' // Assuming your environment variables are stored in a.env file
+})
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) =>{
     res.send('Hello, World!');
 })
+
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
